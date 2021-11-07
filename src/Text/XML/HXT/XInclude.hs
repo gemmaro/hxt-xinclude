6164a2f63531ccb7d80082b2b985df1587005b8e
gemmaro <gemmaro.dev@gmail.com>
2021-11-07 10:18:21 +0900

    [ refactor ] Split into smaller functions

--- src/Text/XML/HXT/XInclude.hs
+++ src/Text/XML/HXT/XInclude.hs
@@ -4,27 +4,30 @@ import System.FilePath (takeDirectory, (</>))
 import Text.XML.HXT.Core
 
 extract :: IOSArrow XmlTree XmlTree
-extract =
-  propagateNamespaces
-    >>> ( ( \s ->
-              processBottomUp
-                ( ( ( \(h, p) ->
-                        let h' :: String
-                            h' = takeDirectory s </> h
-                         in choiceA
-                              [ isA (\_ -> p == "text") :-> (txt $< arrIO (\_ -> readFile h')),
-                                isA (\_ -> p == "xml" || p == "")
-                                  :-> ( readDocument [] h'
-                                          >>> propagateNamespaces
-                                          >>> extract
-                                          >>> getChildren
-                                      )
-                              ]
-                    )
-                      $< (getAttrValue "href" &&& getAttrValue "parse")
-                  )
-                    `when` hasQName (mkQName "xi" "include" "http://www.w3.org/2001/XInclude")
+extract = propagateNamespaces >>> (processWithSourcePath $< getAttrValue "source")
+
+processWithSourcePath :: FilePath -> IOSArrow XmlTree XmlTree
+processWithSourcePath s =
+  processBottomUp $
+    (extractedChildren s $< getAttrValue "href" &&& getAttrValue "parse")
+      `when` hasQName (mkQName "xi" "include" "http://www.w3.org/2001/XInclude")
+
+extractedChildren ::
+  -- | File path of parent document
+  FilePath ->
+  -- | XInclude's @href@ and @parse@ attribute values
+  (String, String) ->
+  -- | Arrow to read file
+  IOSArrow XmlTree XmlTree
+extractedChildren s (h, p) =
+  let h' :: FilePath
+      h' = takeDirectory s </> h
+   in choiceA
+        [ isA (const $ p == "text") :-> (txt $< arrIO0 (readFile h')),
+          isA (const $ p == "xml" || p == "")
+            :-> ( readDocument [] h'
+                    >>> propagateNamespaces
+                    >>> extract
+                    >>> getChildren
                 )
-          )
-            $< getAttrValue "source"
-        )
+        ]
